eb7c76a2b0535b52046f40885049f6887e5485a1
Andres Lot Camarena <lotcamandres@gmail.com>
2024-10-17 02:11:04 +0200

    refactor to structs for Point3D and Point2D

--- src/utils.c
+++ src/utils.c
@@ -4,36 +4,38 @@
 #include <unistd.h>
 #include "utils.h"
 
-float cube_vertices[8][3] = {
-	{-1.0, -1.0, -1.0},
-	{1.0, -1.0, -1.0},
-	{1.0, 1.0, -1.0},
-	{-1.0, 1.0, -1.0},
-	{-1.0, -1.0, 1.0},
-	{1.0, -1.0, 1.0},
-	{1.0, 1.0, 1.0},
-	{-1.0, 1.0, 1.0}
-};
 
 void screen_clear() {
 	printf("\x1b[2J");
 	printf("\x1b[H");
 }
 
-void project_3D_to_2D(float x, float y, float z, int *x_2d, int *y_2d) {
-	float distance = 2.0;
-	float factor = distance / (distance + z);
-	*x_2d = (int)(x * factor * 5) + (SCREEN_WIDTH / 2.0);
-	*y_2d = (int)(y * factor * 5) + (SCREEN_HEIGHT / 2.0);
+void project_3D_to_2D(Point3D point3D, Point2D *point2D) {
+	float distance = 5.0;
+	float factor = distance / (distance + point3D.z);
+	point2D->x = (int)(point3D.x * factor * 5) + (SCREEN_WIDTH / 2.0);
+	point2D->y = (int)(point3D.y * factor * 5) + (SCREEN_HEIGHT / 2.0);
 }
 
 void draw_cube() {
-	int x_2d[8], y_2d[8]; 
+	
+	Point3D cube_vertices[] = {
+		{-1, -1, -1},
+		{1, -1, -1},
+		{1, 1, -1},
+		{-1, 1, -1},
+		{-1, -1, 1},
+		{1, -1, 1},
+		{1, 1, 1},
+		{-1, 1, 1}
+	};
+
+	Point2D cube_vertices_2D[8];
 	for (int i = 0; i < 8; i++) {
-		project_3D_to_2D(cube_vertices[i][0], cube_vertices[i][1], cube_vertices[i][2], &x_2d[i], &y_2d[i]);
+		project_3D_to_2D(cube_vertices[i], &cube_vertices_2D[i]);
 	}
 	for (int i = 0; i < 8; i++) {
-		printf("\x1b[%d;%dH", y_2d[i], x_2d[i]);
+		printf("\x1b[%d;%dH", (int)cube_vertices_2D[i].y, (int)cube_vertices_2D[i].x); 
 		printf("@");
 	}
 	printf("\x1b[0;0H");
--- src/utils.h
+++ src/utils.h
@@ -4,8 +4,17 @@
 #define SCREEN_WIDTH 120 
 #define SCREEN_HEIGHT 40
 
+
+typedef struct {
+	float x, y, z;
+} Point3D;
+
+typedef struct {
+	float x, y;
+} Point2D;
+
 void screen_clear(void);
-void project_3D_to_2D(float x, float y, float z, int *x_2d, int *y_2d);
+void project_3D_to_2D(Point3D point3D, Point2D *point2D);
 void draw_cube(void);
 
 #endif
